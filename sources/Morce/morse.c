ecffd2e4aeb85b5902b8dae929a8a55ac0a1ed79
nakyto <theo.berguig@free.fr>
2020-03-24 18:02:39 +0100

    modification du code fonctonel Morce.c

--- sources/Morce/morse.c
+++ sources/Morce/morse.c
@@ -1,25 +1,157 @@
-
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 
 enum alphabet { space, a, b, c, d, e, f, g, h ,i, j, k, l, m, n, o, p, q, r, s, t, u, v, w, x, y, z};
 
-int main()
-{
-    char alpha[27] = {' ', 'a', 'b', 'c', 'd', 'e'};
-    char* morce[] = {"|", ".-", "-...", "-.-.", "-..", "."};
 
-    char test[20] = "a abc";
+/**
+ * @note : L'utilisateur, va entré dans un "input" pour traduire la chaine de charactère
+*/
+int main(int argc, char *argv[]){
+  char ALFABET[27] = {' ', 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'};
+  char* morce[] = {"|", ".-", "-...", "-.-.", "-..", ".", "..-.", "--.","....", "..", ".---", "-.-", ".-..", "--", "-.", "---", ".--.", "--.-", ".-.", "...", "-", "..-", "...-", ".--", "-..-", "-.--", "--.."} ;
 
-    for(int i = 0; i < 10; i++)
+  char LA_CHAINE_A_CONSERTIRE[255];
+
+  printf("==============================|\n");
+  printf("------------MORCE-------------|\n");
+  printf("Information importante :      |\n");
+  printf("dans votre résulta les '!'    |\n");
+  printf("son des charactre non pris    |\n");
+  printf("en charge, veiller indique,   |\n");
+  printf("UNIQUEMENT des letre + espace |\n");
+  printf("===============================\n");
+
+  printf("Enter an String :");
+  fgets(LA_CHAINE_A_CONSERTIRE,255,stdin);
+  printf("ma chaine :%s",LA_CHAINE_A_CONSERTIRE);
+
+  int longeur_chain = strlen(LA_CHAINE_A_CONSERTIRE);
+  int i, j, cpt = 0;
+
+  for(i = 0 ; i<longeur_chain;i++){
+
+
+    switch (LA_CHAINE_A_CONSERTIRE[i])
     {
-        for (int j = 0; j < 27; j++)
-        {
-            if(test[i] == alpha[j])
-            {
-                printf("%s ", morce[j]);
-            }
-        }
+    case ' ':
+      printf("%s",morce[space]);
+      //strncpy(maConvertion,&morce_space,sizeof(LA_CHAINE_A_CONSERTIRE[i]));
+      break;
+
+    case 'a':
+      printf("%s",morce[a]);
+      break;
+
+    case 'b':
+      printf("%s",morce[b]);
+      break;
+
+    case 'c':
+      printf("%s",morce[c]);
+      break;
+
+    case 'd':
+      printf("%s",morce[d]);
+      break;
+
+    case 'e':
+      printf("%s",morce[e]);
+      break;
+
+    case 'f':
+      printf("%s",morce[f]);
+      break;
+
+    case 'g':
+      printf("%s",morce[g]);
+      break;
+
+    case 'h':
+      printf("%s",morce[h]);
+      break;
+
+    case 'i':
+      printf("%s",morce[i]);
+      break;
+
+    case 'j':
+      printf("%s",morce[j]);
+      break;
+
+    case 'k':
+      printf("%s",morce[k]);
+      break;
+
+    case 'l':
+      printf("%s",morce[l]);
+      break;
+
+    case 'm':
+      printf("%s",morce[m]);
+      break;
+
+    case 'n':
+      printf("%s",morce[n]);
+      break;
+
+    case 'o':
+      printf("%s",morce[o]);
+      break;
+
+    case 'p':
+      printf("%s",morce[p]);
+      break;
+
+    case 'q':
+      printf("%s",morce[q]);
+      break;
+
+    case 'r':
+      printf("%s",morce[r]);
+      break;
+
+    case 's':
+      printf("%s",morce[s]);
+      break;
+
+    case 't':
+      printf("%s",morce[t]);
+      break;
+
+    case 'u':
+      printf("%s",morce[u]);
+      break;
+
+    case 'v':
+      printf("%s",morce[v]);
+      break;
+
+    case 'w':
+      printf("%s",morce[w]);
+      break;
+
+    case 'x':
+      printf("%s",morce[x]);
+      break;
+
+    case 'y':
+      printf("%s",morce[y]);
+      break;
+
+    case 'z':
+      printf("%s",morce[z]);
+      break;
+
+    default:
+    //  par défauf / cas d'ereur, cas non suporté marque un !
+      printf("!");
+      break;
     }
 
-    return 0;
+  }
+
+  printf("\n");
+  return 0;
 }
